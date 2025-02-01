fd854dd68d2509f042f3d99c8fc6df4b8f49e951
ezgikardes <ezgikardes@gmail.com>
2025-02-01 22:01:58 +0100

    feature: add event handler to undo button

---
+++ src/Tab.js
@@ -0,0 +1,9 @@
+export default function Tab({ num, activeTab, onClick }) {
+  return (
+    <button
+      className={activeTab === num ? "tab active" : "tab"}
+      onClick={() => onClick(num)}>
+      Tab {num + 1}
+    </button>
+  );
+}
